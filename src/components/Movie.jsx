9434d24d656da01631eadea07bcf347c173c67ec
Cho Thet <chothet707@gmail.com>
2023-04-24 20:00:56 +0630

    first commit

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import photo from "../img/blue_cloud_2.jpg";
 
 const Movie = ({ name, price }) => {
   return (
-    <div className="text-gray-500">
-      <ul>
-        <li>{name}</li>
-        <li>{price}</li>
-      </ul>
-    </div>
+    // <div className="text-gray-500">
+    //   <ul>
+    //     <li>{name}</li>
+    //     <li>{price}</li>
+    //   </ul>
+    // </div>
+    <div className="w-30 my-4 h-30 rounded shadow bg-gray-300  flex-auto p-1 hover:bg-white">
+       <img src = {photo} alt="" className="mx-auto px-3 my-3" width = "100" height="50"/>
+       <p className="text-center">h1</p>
+     </div>
   );
 };
 
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,14 +1,46 @@
 import Movie from "./Movie";
 import { useContextCustom } from "../context/StateContext";
+import { AiTwotoneBank } from "react-icons/ai";
+import viewphoto from "../img/zoom_background2.jpg";
 
 const MovieList = () => {
   // const { movieLists, setMovieLists } = useContext(StateContext);
   const { movieLists, setMovieLists } = useContextCustom();
   return (
-    <div>
-      {movieLists.map((movieList) => {
-        return <Movie key={movieList.id} {...movieList} />;
-      })}
+    <div calssName = "flex space-x-4 mb-6 text-sm font-medium ">
+      <div className="flex-auto flex space-x-4 shadow flex-wrap px-8 ">
+            <div className="flex bg-blue-400">
+                    <div className="flex-auto  w-64">
+                      <ul>
+                        <li>Health & Beauty</li>
+                        <li>Electronic Devices</li>
+                        <li>Electronics Accessories</li>
+                        <li>TV and Home Appliances</li>
+                        <li>Women's Fashion</li>
+                        <li>Men's Fashion</li>
+                      </ul>
+                    </div>
+                    
+                    
+                        
+                        <div className="w-5/6 bg-red-400" >
+                          <div className=" mx-5"><img src = {viewphoto} ></img></div>
+                        </div>
+                        
+              </div>           
+                    
+
+                   
+            <div class="w-full text-sm font-medium text-slate-700 mt-2 ms-16">
+               <button><a href = "" className="flex rounded-full w-32 bg-gray-400 py-3 px-2 text-white mx-5"><AiTwotoneBank/>Official Stores</a></button>
+               <button><a href = "" className="flex rounded-full w-32 bg-gray-400 py-3 px-2 text-white mx-5"><AiTwotoneBank/>Shop Mart</a></button>
+               <button><a href = "" className="flex rounded-full w-45 bg-gray-400 py-3 px-2 text-white mx-5"><AiTwotoneBank/>Express Delivery</a></button>
+               <button><a href = "" className="flex rounded-full w-50 bg-gray-400 py-3 px-2 text-white mx-5"><AiTwotoneBank/>Top Up/E-Store</a></button> 
+            </div>   
+          {movieLists.map((movieList) => {
+            return <Movie key={movieList.id} {...movieList} />;
+          })}
+      </div>
     </div>
   );
 };
